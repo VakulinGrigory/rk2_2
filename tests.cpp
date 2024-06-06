2deeefbc96946e37c626d6f10a9f7c66c7d0cef2
VakulinGrigory <github.vakulingd@mail.ru>
2024-06-06 09:26:44 +0300

    11

--- tests.cpp
+++ tests.cpp
@@ -1,35 +1,29 @@
+#include <gtest/gtest.h>
 #include "BusinessSystem.cpp"
-#include "gtest/gtest.h"
 
 // Test case for EstateOwner class
 TEST(EstateOwnerTest, SetEstateRentPriceTest) {
-    EstateOwner estateOwner;
-    estateOwner.SetEstateRentPrice(20000);
-    // Now we need to add a method to get the rent price
-    EXPECT_EQ(estateOwner.GetEstateRentPrice(), 20000);
+    EstateOwner owner;
+    BusinessMediator mediator(owner, GroceryStore(), Restaurant());
+
+    owner.SetEstateRentPrice(1000);
+    ASSERT_EQ(owner.GetRentPrice(), 1000);
 }
 
 // Test case for GroceryStore class
 TEST(GroceryStoreTest, SupplyTest) {
-    GroceryStore groceryStore;
-    groceryStore.Supply(10);
-    // Now we need to add a method to get the stock
-    EXPECT_EQ(groceryStore.GetStock(), 10);
-}
+    GroceryStore store;
+    BusinessMediator mediator(EstateOwner(), store, Restaurant());
 
-// Since AlterPrice method already exists in GroceryStore, there is no need to fix it.
+    store.Supply(100);
+    ASSERT_EQ(store.GetStock(), 100);
+}
 
 // Test case for Restaurant class
-TEST(RestaurantTest, SetIsOpenedTest) {
+TEST(RestaurantTest, CookFoodTest) {
     Restaurant restaurant;
+    BusinessMediator mediator(EstateOwner(), GroceryStore(), restaurant);
+
     restaurant.SetIsOpened(true);
-    // Now we need to add a method to check if the restaurant is opened
-    EXPECT_TRUE(restaurant.IsOpened());
-}
-
-// Since CookFood method already exists in Restaurant, there is no need to fix it.
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    ASSERT_GT(restaurant.CookFood(), 0);
 }
