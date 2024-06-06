169bc04a40fcec587172b44585eb255841e87071
VakulinGrigory <github.vakulingd@mail.ru>
2024-06-06 09:19:54 +0300

    updated tests

--- tests.cpp
+++ tests.cpp
@@ -1,38 +1,21 @@
-#include <gtest/gtest.h>
 #include "BusinessSystem.cpp"
+#include "gtest/gtest.h"
 
-TEST(BusinessTest, EstateRentPriceChangeTest) {
+// Test case for EstateOwner class
+TEST(EstateOwnerTest, SetEstateRentPriceTest) {
     EstateOwner estateOwner;
-    GroceryStore groceryStore;
-    Restaurant restaurant;
-    BusinessMediator mediator(estateOwner, groceryStore, restaurant);
-
     estateOwner.SetEstateRentPrice(20000);
-    EXPECT_EQ(groceryStore.AlterPrice(0), 101); // Expected grocery price change
-    EXPECT_EQ(restaurant.AlterPrice(0), 510); // Expected restaurant price change
+    EXPECT_EQ(estateOwner.GetEstateRentPrice(), 20000);
 }
 
-TEST(BusinessTest, GroceryStockChangeTest) {
-    EstateOwner estateOwner;
+// Test case for GroceryStore class
+TEST(GroceryStoreTest, SupplyTest) {
     GroceryStore groceryStore;
-    Restaurant restaurant;
-    BusinessMediator mediator(estateOwner, groceryStore, restaurant);
-
     groceryStore.Supply(10);
-    EXPECT_NO_THROW(restaurant.CookFood()); // Restaurant should be open
-
-    groceryStore.Supply(-10);
-    EXPECT_EQ(restaurant.CookFood(), -1); // Restaurant should be closed
+    EXPECT_EQ(groceryStore.GetStock(), 10);
 }
 
-TEST(BusinessTest, FoodIsCookedTest) {
-    EstateOwner estateOwner;
+TEST(GroceryStoreTest, AlterPriceTest) {
     GroceryStore groceryStore;
-    Restaurant restaurant;
-    BusinessMediator mediator(estateOwner, groceryStore, restaurant);
-
-    groceryStore.Supply(1);
-    EXPECT_NO_THROW(restaurant.CookFood()); // Should not throw, as grocery is available
-
-    EXPECT_THROW(groceryStore.Sell(), std::logic_error); // Out of stock
+    EXPECT_EQ(groceryStore.AlterPrice(10), 10);
 }
